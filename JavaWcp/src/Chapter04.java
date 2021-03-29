01989741187c51d09546fa644da7160c510ed6ff
gmgn120 <gmgn120@gmail.com>
2021-03-29 15:08:24 +0900

    初コミット

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1 = "kohei";
+		System.out.println(str1);
+		str1 = "ozu";
+		System.out.println(str1);
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 922337203685477580L;
+		System.out.println(long1);
+		double double1 = 1234567.89;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+
+		Float float1 = 0.123f;
+		System.out.println(float1);
+		Integer int1 = 3;
+		System.out.println(int1);
+		Character car1 = 'あ';
+		System.out.println(car1);
+
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+	}
+}
