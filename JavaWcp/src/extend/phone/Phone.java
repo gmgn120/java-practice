babb1dd969dccd06370a669c19c3675dc160c091
gmgn120 <gmgn120@gmail.com>
2021-03-31 17:11:54 +0900

    継承練習2

--- JavaWcp/src/extend/phone/Phone.java
+++ JavaWcp/src/extend/phone/Phone.java
@@ -1,7 +1,11 @@
 package extend.phone;
 
 public interface Phone {
-    int MAX_NUMBER_DIGITS = 11;
+	int MAX_NUMBER_DIGITS = 11;
 
-    public void call(String number);
+	public void call(String number);
+
+	default void powerOff() {
+		System.out.println("電源を切ります。");
+	}
 }
