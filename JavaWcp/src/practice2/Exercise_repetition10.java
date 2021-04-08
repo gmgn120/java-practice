3c9566fac15b70d126ece670ef28fd20a9190aac
gmgn120 <gmgn120@gmail.com>
2021-04-08 13:43:15 +0900

    九九表出力

---
+++ JavaWcp/src/practice2/Exercise_repetition10.java
@@ -0,0 +1,15 @@
+package practice2;
+
+
+public class Exercise_repetition10 {
+	public static void main(String[] args) {
+		for (int p = 1;p <= 9;p++) {
+			for (int q= 1; q <= 9;q++) {
+				System.out.printf(" %2d", p * q );
+			}
+			System.out.println();
+		}
+	}
+}
+
+
