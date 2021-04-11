cbbcb416b458d30deb0b7ee4982b2ebb98bd9935
gmgn120 <gmgn120@gmail.com>
2021-04-11 14:27:17 +0900

    メソッド練習

---
+++ JavaWcp/src/practice2/Exercise_method.java
@@ -0,0 +1,10 @@
+package practice2;
+
+public class Exercise_method {
+	public static void main(String[] args) {
+		System.out.println("3の２乗は"+ pow2(3));
+	}
+	static int pow2(int n) {
+		return n *n;
+	}
+}
