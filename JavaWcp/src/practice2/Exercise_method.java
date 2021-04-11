fe32f21e171ec35fd105e54d45793c50446e6c7b
gmgn120 <gmgn120@gmail.com>
2021-04-11 14:40:39 +0900

    メソッド練習3

--- JavaWcp/src/practice2/Exercise_method.java
+++ JavaWcp/src/practice2/Exercise_method.java
@@ -2,8 +2,10 @@ package practice2;
 
 public class Exercise_method {
 	public static void main(String[] args) {
+		int z = 8;
 		System.out.println("3の２乗は"+ pow2(3));
 		System.out.println("平均は"+average(3,4));
+		System.out.println("最も大きいのは"+ large(z,large(5,6)));
 	}
 	static int pow2(int n) {
 		return n *n;
@@ -11,5 +13,13 @@ public class Exercise_method {
 
 	static int average(int n,int m) {
 		return (n+m)/2;
+	}
+	
+	static int large(int x, int y) {
+		if ( x < y) {
+			return y;
+		}else {
+			return x;
+		}
 	}
 }
