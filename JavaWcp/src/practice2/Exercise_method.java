a3c53e514a2819625612d11c4b427b48904354c3
gmgn120 <gmgn120@gmail.com>
2021-04-11 14:34:13 +0900

    メソッド練習2

--- JavaWcp/src/practice2/Exercise_method.java
+++ JavaWcp/src/practice2/Exercise_method.java
@@ -3,8 +3,13 @@ package practice2;
 public class Exercise_method {
 	public static void main(String[] args) {
 		System.out.println("3の２乗は"+ pow2(3));
+		System.out.println("平均は"+average(3,4));
 	}
 	static int pow2(int n) {
 		return n *n;
+	}
+
+	static int average(int n,int m) {
+		return (n+m)/2;
 	}
 }
