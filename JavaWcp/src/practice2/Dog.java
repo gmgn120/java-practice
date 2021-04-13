8fe34307123e627a9bcef357f270cb32b512c4fc
gmgn120 <gmgn120@gmail.com>
2021-04-13 16:18:56 +0900

    クラス練習問題

--- JavaWcp/src/practice2/Dog.java
+++ JavaWcp/src/practice2/Dog.java
@@ -2,10 +2,13 @@ package practice2;
 
 public class Dog {
 	private String Name;
+
 	public void SetName(String nm) {
 		Name = nm;
 	}
+
 	public void ShowProfile() {
-		System.out.println("私の名前は"+Name+"です");
+		System.out.println("私の名前は" + Name + "です");
 	}
+
 }
---
+++ JavaWcp/src/practice2/Exercise.java
@@ -0,0 +1,9 @@
+package practice2;
+
+public class Exercise {
+	public static void main(String[] args) {
+		Dog mydog = new Dog();
+		mydog.SetName("エリー");
+		mydog.ShowProfile();
+	}
+}
