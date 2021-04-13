04432f4124756ca539b21740e301076a24222c5f
gmgn120 <gmgn120@gmail.com>
2021-04-13 16:12:30 +0900

    クラス練習問題

---
+++ JavaWcp/src/practice2/Dog.java
@@ -0,0 +1,11 @@
+package practice2;
+
+public class Dog {
+	private String Name;
+	public void SetName(String nm) {
+		Name = nm;
+	}
+	public void ShowProfile() {
+		System.out.println("私の名前は"+Name+"です");
+	}
+}
