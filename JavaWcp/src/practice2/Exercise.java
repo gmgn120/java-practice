acca82d803fcc6dfbe6739f3326d2256815e995b
gmgn120 <gmgn120@gmail.com>
2021-04-13 16:31:26 +0900

    クラス練習問題

--- JavaWcp/src/practice2/Exercise.java
+++ JavaWcp/src/practice2/Exercise.java
@@ -3,8 +3,12 @@ package practice2;
 public class Exercise {
 	public static void main(String[] args) {
 		Dog mydog = new Dog();
+		Dog yourdog = new Dog();
 		mydog.setName("エリー");
 		mydog.setAge(3);
+		yourdog.setName("ジョン");
+		yourdog.setAge(4);
 		mydog.ShowProfile();
+		yourdog.ShowProfile();
 	}
 }
