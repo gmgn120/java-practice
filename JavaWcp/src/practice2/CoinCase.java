22d012107eb74c2991780c1c47ead38c9425ede7
gmgn120 <gmgn120@gmail.com>
2021-04-15 15:08:25 +0900

    GetAmountメソッド作成

--- JavaWcp/src/practice2/CoinCase.java
+++ JavaWcp/src/practice2/CoinCase.java
@@ -48,6 +48,12 @@ public class CoinCase {
     	return 0;
     }
 
+    public int GetAmount() {
+    	return ( Yen500 * 500 ) + ( Yen100 * 100 )
+                + ( Yen50 * 50 ) + ( Yen10 * 10 )
+                + ( Yen5 * 5 ) + Yen1;
+    }
+
 
 }
 
