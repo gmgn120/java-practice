74bf215ccc5e534252a89cddcdc78419284e92f1
gmgn120 <gmgn120@gmail.com>
2021-04-15 15:01:47 +0900

    AddCoinsメソッド作成

--- JavaWcp/src/practice2/CoinCase.java
+++ JavaWcp/src/practice2/CoinCase.java
@@ -1,5 +1,36 @@
 package practice2;
 
 public class CoinCase {
+	private int Yen500 = 0;
+	private int Yen100 = 0;
+	private int Yen50 = 0;
+    private int Yen10 = 0;
+    private int Yen5 = 0;
+    private int Yen1 = 0;
+
+    public void AddCoins(int kind, int count) {
+    	switch(kind) {
+    	case 500:
+    		Yen500 += count;
+    		break;
+    	case 100:
+    		Yen100 += count;
+    		break;
+    	case 50:
+    		Yen50 += count;
+    		break;
+    	case 10:
+    		Yen10 += count;
+    		break;
+    	case 5:
+    		Yen5 += count;
+    		break;
+    	case 1:
+    		Yen1 += count;
+    		break;
+    	}
+    }
+
 
 }
+
