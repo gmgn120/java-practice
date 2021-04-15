38a7e5d6ec4a1517ac48dcf5bce00f6f2cf512c8
gmgn120 <gmgn120@gmail.com>
2021-04-15 15:06:53 +0900

    GetCountメソッド作成

--- JavaWcp/src/practice2/CoinCase.java
+++ JavaWcp/src/practice2/CoinCase.java
@@ -30,6 +30,23 @@ public class CoinCase {
     		break;
     	}
     }
+    public int GetCount(int kind) {
+    	switch(kind) {
+    	case 500:
+    		return Yen500;
+    	case 100:
+    		return Yen100;
+    	case 50:
+    		return Yen50;
+    	case 10:
+    		return Yen10;
+    	case 5:
+    		return Yen5;
+    	case 1:
+    		return Yen1;
+    	}
+    	return 0;
+    }
 
 
 }
